33e1ad921a619cf7b811a57fbc8cb4acfde1164f
carki <94.nogari@gmail.com>
2023-10-04 17:09:09 +0900

    [Init] Init Commit

---
+++ SwiftUI_UI_Challenge/SwiftUI_UI_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_UI_ChallengeApp.swift
+//  SwiftUI_UI_Challenge
+//
+//  Created by HESSEGG on 2023/10/04.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_UI_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
